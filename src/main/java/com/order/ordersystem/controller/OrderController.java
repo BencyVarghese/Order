7d8b9c109ab46b040a13d865a36051924142c709
Bency Varghese <bency.varun@gmail.com>
2024-06-16 13:12:58 +0200

    controller edit

--- src/main/java/com/order/ordersystem/controller/OrderController.java
+++ src/main/java/com/order/ordersystem/controller/OrderController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderController {
 	@GetMapping("/order")
 	public String getOrder() {
-		return "Hello Bency";
+		return "Hello Bency6";
 				
 	}
 
